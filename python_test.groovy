882deae8061cfbb6dedc382bbd520c1b7cb603f9
ThanushaW <thanushaw@outlook.com>
2023-07-14 06:59:18 +0530

    syntax fix

--- python_test.groovy
+++ python_test.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 script {
                     // Run the Python script and capture the output
-                    def output = sh(script: 'python script.py', returnStdout: true).trim()
+                    def output = sh(script: 'python3 script.py', returnStdout: true).trim()
                     
                     // Print the captured output
                     echo "Python script output:\n${output}"
